d115371fabdba8d35e505c9110fe05483236ce44
dominiknair <dominiknair@gmail.com>
2021-06-12 15:15:34 +0100

    added some extra styles and list in itemsDetails

--- js/controllers/ItemDetailsController.js
+++ js/controllers/ItemDetailsController.js
@@ -1,4 +1,32 @@
 checkoutApp.controller("ItemDetailsController", 
     function ItemDetailsController($scope){
         $scope.message = "item details page";
+
+
+        // $scope.items ={
+        //     name: "toy",
+        //     price:55
+        // }
+        $scope.items={
+            firstItem : {
+                name: "lego",
+                price: 10.99,
+                age: 4
+            },
+            secondItem : {
+                name: "bouncy ball",
+                price: 4,
+                age:2
+            },
+            thirdItem : {
+                name: "hotwheels car",
+                price: 2,
+                age:4
+            },
+            fourthItem : {
+                name: "action man",
+                price: 15.99,
+                age:5
+            }
+        }
     })
