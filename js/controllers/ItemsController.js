fa01cae885023b0eab29e116d014d60562a47890
dominiknair <dominiknair@gmail.com>
2021-06-12 12:49:56 +0100

    updated $scope.message and added form to html

--- js/controllers/ItemsController.js
+++ js/controllers/ItemsController.js
@@ -3,6 +3,6 @@
 checkoutApp.controller("ItemsController", 
     function ItemsController($scope)
     {
-        $scope.message="there is an error in my file";
+        $scope.message="Checkout form";
     }
 );
