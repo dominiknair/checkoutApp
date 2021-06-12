a1a42a19c2b87600e21570bf6b2c360417313fca
dominiknair <dominiknair@gmail.com>
2021-06-12 16:14:22 +0100

    added sort form on html

--- js/controllers/ItemDetailsController.js
+++ js/controllers/ItemDetailsController.js
@@ -11,22 +11,34 @@ checkoutApp.controller("ItemDetailsController",
             firstItem : {
                 name: "lego",
                 price: 10.99,
-                age: 4
+                age: 4,
+                upvoteCount:0
             },
             secondItem : {
                 name: "bouncy ball",
                 price: 4,
-                age:2
+                age:2,
+                upvoteCount:0
             },
             thirdItem : {
                 name: "hotwheels car",
                 price: 2,
-                age:4
+                age:4,
+                upvoteCount:0
             },
             fourthItem : {
                 name: "action man",
                 price: 15.99,
-                age:5
+                age:5,
+                upvoteCount:0
             }
+        }
+        $scope.upVote = function (item)
+        {
+            item.upvoteCount++;
+        }
+        $scope.downVote =function (item)
+        {
+            item.upvoteCount--;
         }
     })
